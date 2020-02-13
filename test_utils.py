14389a42912d9ba639825ca0e7cf03f77d149227
wasanico <salocin.sam@gmail.com>
2020-02-13 09:55:30 +0100

    j ai enfin compris

---
+++ test_utils.py
@@ -0,0 +1,24 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 8, 2018
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_fact(self):
+        # À compléter...
+        pass
+    
+    def test_roots(self):
+        # À compléter...
+        pass
+    
+    def test_integrate(self):
+        # À compléter...
+        pass
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
